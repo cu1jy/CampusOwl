b9d734923bee79dea20e6ebd27cd432b2ce5fb04
Allison <cuijy@umich.edu>
2024-09-28 18:44:01 -0400

    services

---
+++ app/home/page.js
@@ -0,0 +1,18 @@
+"use client";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+    return (
+        <main className="min-h-screen bg-gray-100">
+            <Navbar></Navbar>
+            <div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </main>
+    )
+}
