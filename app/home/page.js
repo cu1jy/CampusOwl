2e4d1584365e95d11c0e75f5d9744baeeca87e65
Allison <cuijy@umich.edu>
2024-09-28 19:29:11 -0400

    workgin on home

--- app/home/page.js
+++ app/home/page.js
@@ -6,11 +6,12 @@ export default function Home() {
         <main className="min-h-screen bg-gray-100">
             <Navbar></Navbar>
             <div>
-                <div>
-
+                <div className="container mx-auto py-4">
+                    <h1 className="font-semibold pt-10 text-black text-3xl text-center mb-10">
+                        Home
+                    </h1>
                 </div>
                 <div>
-
                 </div>
             </div>
         </main>
